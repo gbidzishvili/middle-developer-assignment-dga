f19fa9e69922f932550b22202f4df100cc46b426
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 11:29:07 +0400

    important modules added with initial template setup

--- src/app/views/movies-pg/movies-pg.component.ts
+++ src/app/views/movies-pg/movies-pg.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-movies-pg',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, MatIconModule, MatCardModule],
   templateUrl: './movies-pg.component.html',
-  styleUrl: './movies-pg.component.scss'
+  styleUrl: './movies-pg.component.scss',
 })
 export class MoviesPgComponent {
-
+  requestURL = `https://online-movie-database.p.rapidapi.com/title/v2/
+  find?title=starwars&amp;
+  limit=20&amp;
+  paginationKey=0&amp;
+  sortArg=moviemeter%2Casc`;
 }
