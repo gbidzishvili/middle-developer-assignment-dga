b7b436da111be66e5e016adecc1f6cc52b7e546b
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 17:53:15 +0400

    base-proxy service added

--- src/app/core/services/base-proxy.service.ts
+++ src/app/core/services/base-proxy.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, retry } from 'rxjs';
+import { Observable, retry, distinctUntilChanged, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,22 +10,13 @@ export class BaseProxyService {
   get<T>(apiUrl: string, filters?: Object): Observable<T> {
     return this.http
       .get<T>(apiUrl + this.queryParams(filters))
-      .pipe<T>(retry(1)) as Observable<T>;
+      .pipe(shareReplay(), retry(1)) as Observable<T>;
   }
   queryParams(filters: Object | undefined): string {
     if (filters) {
       let params = '?';
-      for (let key in filters) {
-        let filtersValue = filters[key as keyof typeof filters];
-        if (Array.isArray(filtersValue)) {
-          for (let index = 0; index < filtersValue.length; index++) {
-            params += `${key}=${filtersValue[index]}&`;
-          }
-        } else {
-          for (const [key, value] of Object.entries(filtersValue)) {
-            params += `${key}=${value}&`;
-          }
-        }
+      for (let [key, value] of Object.entries(filters)) {
+        params += `${key}=${value}&`;
       }
       return params.slice(0, params.length - 1);
     } else {
