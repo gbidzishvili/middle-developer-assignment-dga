8659211a17f41e0d8764c455af998b7a07cdd997
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 10:51:35 +0400

    string comparison solution with 2 ways

--- src/app/views/string-comparison-pg/string-comparison-pg.component.ts
+++ src/app/views/string-comparison-pg/string-comparison-pg.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-// import * as stringComparison from 'string-comparison';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { stringSimilarity } from 'string-similarity-js';
 @Component({
   selector: 'app-string-comparison-pg',
   standalone: true,
@@ -9,67 +10,82 @@ import { Component, Input } from '@angular/core';
   styleUrl: './string-comparison-pg.component.scss',
 })
 export class StringComparisonPgComponent {
-  @Input() value1 = 'gio';
-  @Input() value2 = ['gio234', '00', 'gio', 'sdf2ggg', 'sdfg', 'ig'];
-
-  matches: { value: string; percentage: number }[] = [];
-
-  // constructor() {}
-
+  stringToCompare = 'st';
+  strings = ['stosdf', 'st', 'sto', '00'];
+  comparedByNpmPackage = new BehaviorSubject<
+    { str1: string; str2: string; percentage: number }[]
+  >([]);
+  comparedByLevenshteinSimilarity = new BehaviorSubject<
+    { str1: string; str2: string; percentage: number }[]
+  >([]);
   ngOnInit(): void {
-    // const Thanos = 'gi';
-    // const Rival = 'gigi';
-    // const Avengers = ['edward', 'sealed', 'theatre'];
-    // // use by cosine
-    // let cos = stringComparison.cosine;
-    // console.log(cos.similarity(Thanos, Rival));
+    // WAY 1
+    this.comparedByLevenshteinSimilarity.next(
+      this.strings.map((v) => ({
+        str1: this.stringToCompare,
+        str2: v,
+        percentage:
+          stringSimilarity(
+            v,
+            this.stringToCompare,
+            this.stringToCompare.length
+          ) * 100,
+      }))
+    );
+    // Way 2
+    this.calculateMatches();
   }
 
-  // ngOnChanges(): void {
-  //   this.calculateMatches();
-  // }
+  ngOnChanges(): void {}
 
-  // calculateMatches(): void {
-  //   this.matches = this.value2.map((value) => ({
-  //     value: value,
-  //     percentage: this.calculateLevenshteinSimilarity(this.value1, value),
-  //   }));
-  // }
+  calculateMatches(): void {
+    this.comparedByNpmPackage.next(
+      this.strings.map((value) => ({
+        str1: this.stringToCompare,
+        str2: value,
+        percentage: this.calculateLevenshteinSimilarity(
+          this.stringToCompare,
+          value
+        ),
+      }))
+    );
+    console.log(this.comparedByNpmPackage);
+  }
 
-  // calculateLevenshteinDistance(str1: string, str2: string): number {
-  //   const matrix = [];
+  calculateLevenshteinDistance(str1: string, str2: string): number {
+    const matrix = [];
 
-  //   // Initialize the matrix
-  //   for (let i = 0; i <= str1.length; i++) {
-  //     matrix[i] = [i];
-  //   }
-  //   for (let j = 0; j <= str2.length; j++) {
-  //     matrix[0][j] = j;
-  //   }
+    // Initialize the matrix
+    for (let i = 0; i <= str1.length; i++) {
+      matrix[i] = [i];
+    }
+    for (let j = 0; j <= str2.length; j++) {
+      matrix[0][j] = j;
+    }
 
-  //   // Compute the Levenshtein distance
-  //   for (let i = 1; i <= str1.length; i++) {
-  //     for (let j = 1; j <= str2.length; j++) {
-  //       if (str1.charAt(i - 1) === str2.charAt(j - 1)) {
-  //         matrix[i][j] = matrix[i - 1][j - 1];
-  //       } else {
-  //         matrix[i][j] = Math.min(
-  //           matrix[i - 1][j - 1] + 1, // substitution
-  //           Math.min(
-  //             matrix[i][j - 1] + 1, // insertion
-  //             matrix[i - 1][j] + 1
-  //           ) // deletion
-  //         );
-  //       }
-  //     }
-  //   }
+    // Compute the Levenshtein distance
+    for (let i = 1; i <= str1.length; i++) {
+      for (let j = 1; j <= str2.length; j++) {
+        if (str1.charAt(i - 1) === str2.charAt(j - 1)) {
+          matrix[i][j] = matrix[i - 1][j - 1];
+        } else {
+          matrix[i][j] = Math.min(
+            matrix[i - 1][j - 1] + 1, // substitution
+            Math.min(
+              matrix[i][j - 1] + 1, // insertion
+              matrix[i - 1][j] + 1
+            ) // deletion
+          );
+        }
+      }
+    }
 
-  //   return matrix[str1.length][str2.length];
-  // }
+    return matrix[str1.length][str2.length];
+  }
 
-  // calculateLevenshteinSimilarity(str1: string, str2: string): number {
-  //   const distance = this.calculateLevenshteinDistance(str1, str2);
-  //   const maxLength = Math.max(str1.length, str2.length);
-  //   return ((maxLength - distance) / maxLength) * 100;
-  // }
+  calculateLevenshteinSimilarity(str1: string, str2: string): number {
+    const distance = this.calculateLevenshteinDistance(str1, str2);
+    const maxLength = Math.max(str1.length, str2.length);
+    return ((maxLength - distance) / maxLength) * 100;
+  }
 }
