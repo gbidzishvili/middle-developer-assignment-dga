4f43267f4f8d1f6a594cd27665afa395c19d9b5d
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 11:05:02 +0400

    movies pg added

---
+++ src/app/views/movies-pg/movies-pg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-pg',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-pg.component.html',
+  styleUrl: './movies-pg.component.scss'
+})
+export class MoviesPgComponent {
+
+}
