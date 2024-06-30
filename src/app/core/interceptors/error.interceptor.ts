c24c249e39f712bcee93b48d50853331a4af052c
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 18:54:20 +0400

    error interceptor added

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { NotificationService } from '../services/notification.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const notificationService = inject(NotificationService);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let errorMsg = '';
+      if (error.error instanceof ErrorEvent) {
+        console.log('client side error');
+        errorMsg = `Error: ${error.error.message}`;
+      } else {
+        console.log('server side error');
+        errorMsg = `Error Code: ${error.status}, Message: ${error.message}`;
+      }
+      notificationService.showError(errorMsg);
+      return throwError(() => new Error(errorMsg));
+    })
+  );
+};
