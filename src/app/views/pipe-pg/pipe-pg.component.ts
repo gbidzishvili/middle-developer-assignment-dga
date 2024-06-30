5b511cd08310da2fb9fc5bae257892c580975dc5
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 09:46:53 +0400

    datepipe logic improved

--- src/app/views/pipe-pg/pipe-pg.component.ts
+++ src/app/views/pipe-pg/pipe-pg.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomDatePipe } from './pipes/custom-date.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pipe-pg',
   standalone: true,
-  imports: [CustomDatePipe],
+  imports: [CustomDatePipe, DatePipe],
   templateUrl: './pipe-pg.component.html',
   styleUrl: './pipe-pg.component.scss',
 })
