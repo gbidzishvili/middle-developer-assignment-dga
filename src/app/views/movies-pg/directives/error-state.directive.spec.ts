c18ea3f1f8611ea9d7b882279e85c905a0718f1a
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 21:28:54 +0400

    refactoring

--- src/app/views/movies-pg/directives/error-state.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorStateDirective } from './error-state.directive';
-
-describe('ErrorStateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ErrorStateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
