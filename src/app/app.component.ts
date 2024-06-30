26e6d94e98bb117018b938f7355e5a6db7575491
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-06-30 11:06:19 +0400

    heaer component added to app.compoenent.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsPgComponent } from './views/forms-pg/forms-pg.component';
+import { HeaderComponent } from './navigation/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsPgComponent],
+  imports: [RouterOutlet, FormsPgComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
