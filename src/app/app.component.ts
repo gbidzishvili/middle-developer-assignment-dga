af9fd0f6d5c17f89835047465010cadca00216e2
giorgi <bidzishviligiorgi7@gmail.com>
2024-07-01 12:32:26 +0400

    particles different implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsPgComponent } from './views/forms-pg/forms-pg.component';
 import { HeaderComponent } from './navigation/components/header/header.component';
-
 declare var particlesJS: any;
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,6 +14,13 @@ declare var particlesJS: any;
 export class AppComponent implements OnInit {
   title = 'dga-assignment';
   ngOnInit(): void {
-    particlesJS.load('particles-js', '../assets/images/particles.json', null);
+    this.initParticles();
+  }
+  initParticles() {
+    particlesJS.load(
+      'particles-js',
+      '../assets/particles_library/particles.json',
+      null
+    );
   }
 }
